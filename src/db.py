6717edd0ab49577c4c0014cd924b036fb3f10ee0
Héloïse Feldmann <feldmann.heloise@gmail.com>
2023-03-20 16:34:29 +0100

    clean to python

--- src/db.py
+++ src/db.py
@@ -8,11 +8,17 @@ import numpy as np
 
 dbfile = './data.db'
 
+Portfolio1 = ["AAPL","GOOGL","FB","AMZN"]
+
 def sqlite_access(dbfile):
+    """
+    """
     con = sqlite3.connect(dbfile)
     return con
 
 def random_tickers(con):
+    """
+    """
     cur = con.cursor()
     tickers = cur.execute("SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';").fetchall()
     tickers = tickers[1:]
@@ -21,7 +27,9 @@ def random_tickers(con):
 
 
 def df_per_tickers(ticker, con):
-    df = pd.read_sql(f'SELECT  Date,"Adj Close**" fROM {ticker[0]}',
+    """
+    """
+    df = pd.read_sql(f'SELECT  Date,"Adj Close**" fROM {ticker}',
                        con,
                        parse_dates=['Date'], index_col='Date')
      
@@ -29,11 +37,13 @@ def df_per_tickers(ticker, con):
     end_date = "2023-03-13"
     mask = (df.index > start_date) & (df.index <= end_date)
     df2 = df.loc[mask]
-    df2 = df2.rename(columns={"Adj Close**": f"{ticker[0]}"})
+    df2 = df2.rename(columns={"Adj Close**": f"{ticker}"})
 
     return df2 
 
 def complete_df(choice,con):
+    """
+    """
     final_dataframe = pd.DataFrame()
     for ticker in choice :   
         df2 = df_per_tickers(ticker,con)
@@ -51,6 +61,8 @@ def complete_df(choice,con):
     return final_dataframe
 
 def plot(df2):
+    """
+    """
     df2 = pd.read_pickle("../Datas/test.pkl")
     cols =df2.columns.tolist()
     for col in cols:
@@ -62,6 +74,6 @@ def plot(df2):
 
 
 con = sqlite_access(dbfile)
-choice = random_tickers(con)
-df2 = complete_df(choice,con)
+#choice = random_tickers(con)
+df2 = complete_df(Portfolio1,con)
 plot(df2)
